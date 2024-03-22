c4ad55e8e21a3d3fe2e685275f80008dbd86b04f
Maidah208 <161070899+Maidah208@users.noreply.github.com>
2024-03-23 00:35:00 +0500

    Add files via upload

---
+++ GuessTheNumber/index.ts
@@ -0,0 +1,18 @@
+import inquirer from "inquirer";
+
+let computerGuess = Math.floor((Math.random()*10) + 1);
+
+let guess = await inquirer.prompt([
+    {
+    message: "Guesss any number betwenn 1 to 10:",
+    type: "number",
+    name: "userGuess",}, 
+]);
+
+console.log(`Computer guess: ${computerGuess}`)
+if (guess.userGuess === computerGuess){
+    console.log(`Congratulations! You Win.`); 
+}else {
+    console.log(`You Lose. Try Again`);
+}
+
